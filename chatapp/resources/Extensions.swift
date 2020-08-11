55af55a71088a8e0100fbb9948992ebfcaeea99b
JasonHaque <zahinjason220434@gmail.com>
2020-08-11 20:47:44 +0600

    code added

--- chatapp/resources/Extensions.swift
+++ chatapp/resources/Extensions.swift
@@ -7,3 +7,31 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIView{
+    
+    public var width : CGFloat{
+        return self.frame.size.width
+    }
+    
+    public var height : CGFloat{
+        return self.frame.size.height
+    }
+    
+    public var top : CGFloat{
+        return self.frame.origin.y
+    }
+    
+    public var bottom : CGFloat{
+        return self.frame.size.height + self.frame.origin.y
+    }
+    
+    public var left : CGFloat{
+        return self.frame.origin.x
+    }
+    
+    public var righ : CGFloat{
+        return self.frame.size.width + self.frame.origin.x
+    }
+}
