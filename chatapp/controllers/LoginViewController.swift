4088bd60cdd480efd8588987f68520f43d88e904
JasonHaque <zahinjason220434@gmail.com>
2020-08-11 20:36:57 +0600

    code added

--- chatapp/controllers/LoginViewController.swift
+++ chatapp/controllers/LoginViewController.swift
@@ -9,6 +9,15 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    private let imageView : UIImageView = {
+       
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,6 +25,20 @@ class LoginViewController: UIViewController {
         view.backgroundColor = .white
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
+        
+        //add subviews
+        view.addSubview(imageView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let size = view.frame.size.width/3
+        
+        imageView.frame = CGRect(x: (view.frame.size.width - size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
     }
     
     @objc private func didTapRegister(){
---
+++ chatapp/resources/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 11/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
