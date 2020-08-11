7eb6015c7f35f4063ccb297e17b887dfd69f87d5
JasonHaque <zahinjason220434@gmail.com>
2020-08-11 19:52:45 +0600

    added view controller

---
+++ chatapp/controllers/NewConversationsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewConversationsViewController.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 11/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class NewConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
