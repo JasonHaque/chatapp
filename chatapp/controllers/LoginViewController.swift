d2e2150d094eb4bc1528a5c74e591296e46a2b6c
JasonHaque <zahinjason220434@gmail.com>
2020-08-11 20:21:34 +0600

    added navigation

--- chatapp/controllers/LoginViewController.swift
+++ chatapp/controllers/LoginViewController.swift
@@ -12,19 +12,17 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Log in"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc private func didTapRegister(){
+        let vc = RegisterViewController()
+        vc.title = "Create User Account"
+        navigationController?.pushViewController(vc, animated: false)
     }
-    */
+
 
 }
--- chatapp/controllers/RegisterViewController.swift
+++ chatapp/controllers/RegisterViewController.swift
@@ -13,18 +13,8 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .link
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
