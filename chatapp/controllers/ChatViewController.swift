ae6231a27a9b6448b8f570a52b860bc80678384e
JasonHaque <zahinjason220434@gmail.com>
2020-08-19 19:27:59 +0600

    table view code added

---
+++ chatapp/controllers/ChatViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ChatViewController.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 19/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+    
+
+
+}
--- chatapp/controllers/ConversationsViewController.swift
+++ chatapp/controllers/ConversationsViewController.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 import FirebaseAuth
+import JGProgressHUD
 
 class ConversationsViewController: UIViewController {
+    
+    private let spinner = JGProgressHUD(style: .dark)
+    
+    private let tableView : UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return table
+    }()
+    
+    private let noConversationsLabel : UILabel = {
+       let label = UILabel()
+        label.text = "No Conversations"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21,weight : .medium)
+        label.isHidden = true
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //DatabaseManager.shared.test()
+        view.addSubview(tableView)
+        view.addSubview(noConversationsLabel)
+        setupTableView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         validateAuth()
+        fetchConversations()
         
         
     }
@@ -34,7 +58,43 @@ class ConversationsViewController: UIViewController {
         }
     }
 
+    private func setupTableView(){
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    private func fetchConversations(){
+        
+    }
     
 
+}
+
+extension ConversationsViewController : UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello there"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let vc = ChatViewController()
+        vc.title = "Joey Smaith"
+        vc.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    
 }
 
