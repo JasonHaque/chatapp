a609fe61e48c6cca213c210617283a633bb78d61
JasonHaque <zahinjason220434@gmail.com>
2020-08-20 17:44:50 +0600

    fetching user image from stoprage

--- chatapp/controllers/ProfileViewController.swift
+++ chatapp/controllers/ProfileViewController.swift
@@ -46,14 +46,40 @@ class ProfileViewController: UIViewController {
         
         
         imageView.contentMode = .scaleAspectFill
-        
+        imageView.backgroundColor = .white
         imageView.layer.borderColor = UIColor.white.cgColor
         imageView.layer.borderWidth = 3
         imageView.layer.masksToBounds = true
         
         headerView.addSubview(imageView)
         
+        StorageManager.shared.downloadURL(for: path) {[weak self] result in
+            
+            switch result{
+            case .success(let url):
+                self?.downloadImage(imageView: imageView, url: url)
+            case .failure(let error):
+                print("failed to get download url : \(error)")
+            }
+        }
         return headerView
+    }
+    
+    func downloadImage(imageView : UIImageView ,url : URL){
+        
+        URLSession.shared.dataTask(with: url) { data, _ , error in
+            
+            guard let data = data , error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                imageView.image = image
+            }
+            
+            
+        }.resume()
     }
     
 
--- chatapp/resources/StorageManager.swift
+++ chatapp/resources/StorageManager.swift
@@ -47,6 +47,24 @@ final class StorageManager{
         } )
     }
     
+    
+    public func downloadURL(for path : String ,completion : @escaping(Result<URL,Error>) -> Void){
+        
+        let reference = storage.child(path)
+        
+        reference.downloadURL { url , error in
+            
+            guard let url = url , error == nil else {
+                
+                completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                return
+            }
+            
+            completion(.success(url))
+           
+        }
+    }
+    
     public enum StorageErrors : Error {
         case failedToUpload
          case failedToGetDownloadUrl
