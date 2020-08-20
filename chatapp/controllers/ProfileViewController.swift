dcc13196034ed2cd5f6dcb40afd8f11fab501734
JasonHaque <zahinjason220434@gmail.com>
2020-08-20 17:22:46 +0600

    added header view

--- chatapp/controllers/ProfileViewController.swift
+++ chatapp/controllers/ProfileViewController.swift
@@ -23,7 +23,37 @@ class ProfileViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.tableHeaderView = createTableHeader()
+    }
+    
+    func createTableHeader() -> UIView?{
         
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        
+        let safeEmail = DatabaseManager.safeEmail(emailAddress: email)
+        let fileName = safeEmail + "_profile_picture.png"
+        
+        let path = "images/"+fileName
+        
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.width, height: 300))
+        
+        headerView.backgroundColor = .link
+        
+        let imageView = UIImageView(frame: CGRect(x: (headerView.width-150)/2, y: 75, width: 150, height: 150))
+        
+        
+        imageView.contentMode = .scaleAspectFill
+        
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.borderWidth = 3
+        imageView.layer.masksToBounds = true
+        
+        headerView.addSubview(imageView)
+        
+        return headerView
     }
     
 
