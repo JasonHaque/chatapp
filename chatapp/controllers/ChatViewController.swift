9d3259493ff33cadfb4bfd94af7612be1970d153
JasonHaque <zahinjason220434@gmail.com>
2020-08-20 22:30:02 +0600

    db schema changed

--- chatapp/controllers/ChatViewController.swift
+++ chatapp/controllers/ChatViewController.swift
@@ -27,11 +27,27 @@ struct Sender : SenderType {
 }
 class ChatViewController: MessagesViewController{
     
+    public static let dateFormatter : DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        formatter.timeStyle = .long
+        formatter.locale = .current
+        return formatter
+    }()
     public var isNewConversation = false
     public let otherUserEmail : String
 
     private var messages = [Message]()
-    private let selfSender = Sender(senderId: "Joe Smith", displayName: "1", photoURL: "")
+    private var selfSender : Sender?{
+        
+        guard let email = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        
+        return Sender(senderId: email, displayName: "Joe Smith", photoURL: "")
+        
+    }
+        
     
     init(with email  : String){
         self.otherUserEmail = email
@@ -65,7 +81,16 @@ class ChatViewController: MessagesViewController{
 
 extension ChatViewController : MessagesDataSource,MessagesLayoutDelegate,MessagesDisplayDelegate{
     func currentSender() -> SenderType {
-        return selfSender
+        
+        if let sender = selfSender{
+             return sender
+        }
+        
+        fatalError("self sender is nil email should be cached")
+        
+        return Sender(senderId: "12", displayName: "", photoURL: "")
+       
+        
     }
     
     func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
@@ -83,7 +108,9 @@ extension ChatViewController : InputBarAccessoryViewDelegate{
     
     func inputBar(_ inputBar: InputBarAccessoryView, didPressSendButtonWith text: String) {
         
-        guard !text.replacingOccurrences(of: " ", with: "").isEmpty else {
+        guard !text.replacingOccurrences(of: " ", with: "").isEmpty,
+            let selfSender = self.selfSender ,
+            let messageId = createMessageId() else {
             return
         }
         
@@ -94,10 +121,37 @@ extension ChatViewController : InputBarAccessoryViewDelegate{
         if isNewConversation{
             
             //create Convo in db
+            
+            let message = Message(sender: selfSender,
+                                  messageId: messageId,
+                                  sentDate: Date(),
+                                  kind: .text(text))
+            
+            DatabaseManager.shared.createNewConversation(with: otherUserEmail, firstMessage: message) {  success in
+                if success{
+                    print("message sent")
+                }
+                else{
+                    print("failed to send")
+                }
+            }
         }
         
         else{
             //append to existing one
         }
+    }
+    
+    private func createMessageId() -> String?{
+        
+        //date , otherUserEmail , SenderMail , randomInt
+        
+        let dateString = Self.dateFormatter.string(from: Date())
+        guard let currentUserEmail = UserDefaults.standard.value(forKey: "email") as? String else {
+            return nil
+        }
+        let newId = "\(otherUserEmail)_\(currentUserEmail)_\(dateString)"
+        
+        return newId
     }
 }
