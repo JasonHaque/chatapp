bf010cd9314b933cb64279fcac0396e71b685e42
JasonHaque <zahinjason220434@gmail.com>
2020-08-20 14:50:58 +0600

    added storage manager

---
+++ chatapp/resources/StorageManager.swift
@@ -0,0 +1,55 @@
+//
+//  StorageManager.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 20/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+import FirebaseStorage
+
+final class StorageManager{
+    
+    static let shared = StorageManager()
+    
+    private let storage = Storage.storage().reference()
+    
+    public typealias UploadPictureCompletion = (Result <String , Error>) -> Void
+    
+    public func uploadProfilePicture(with data : Data ,fileName : String ,completion : @escaping UploadPictureCompletion){
+        
+        
+        storage.child("images/\(fileName)").putData(data, metadata: nil,completion: { metadata , error in
+            
+            guard error == nil else{
+                
+                print("Failed to upload data to firebase for picture")
+                
+                completion(.failure(StorageErrors.failedToUpload))
+                return
+            }
+            
+         self.storage.child("images/\(fileName)").downloadURL(completion: {url , error in
+                
+                guard let url = url else {
+                    print("failedToGetDownloadUrl")
+                    completion(.failure(StorageErrors.failedToGetDownloadUrl))
+                    return
+                }
+                
+                let urlString = url.absoluteString
+                
+                print("download url returned \(urlString)")
+                completion(.success(urlString))
+            })
+            
+        } )
+    }
+    
+    public enum StorageErrors : Error {
+        case failedToUpload
+         case failedToGetDownloadUrl
+        
+    }
+}
