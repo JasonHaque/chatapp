db3afa054f25ac33d66a41db0ff17eb9fc7d9dcc
JasonHaque <zahinjason220434@gmail.com>
2020-08-21 13:52:39 +0600

    cell added

---
+++ chatapp/views/ConversationTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationTableViewCell.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 21/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
