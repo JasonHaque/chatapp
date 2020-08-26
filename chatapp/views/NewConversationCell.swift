d0d61ed89b236fa7ad706f702e07214652e5be31
JasonHaque <zahinjason220434@gmail.com>
2020-08-26 19:40:10 +0600

    added new cell

---
+++ chatapp/views/NewConversationCell.swift
@@ -0,0 +1,84 @@
+//
+//  NewConversationCell.swift
+//  chatapp
+//
+//  Created by Sanviraj Zahin Haque on 26/8/20.
+//  Copyright © 2020 Sanviraj Zahin Haque. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SDWebImage
+
+class NewConversationCell: UITableViewCell {
+    
+    static let identifier = "NewConversationCell"
+    
+    private let userImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 50
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
+    private let userNameLabel : UILabel = {
+        
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 21, weight: .semibold)
+        return label
+        
+    }()
+    
+   
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style : style , reuseIdentifier : reuseIdentifier)
+        
+        contentView.addSubview(userImageView)
+        contentView.addSubview(userNameLabel)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        userImageView.frame = CGRect(x: 10,
+                                     y: 10,
+                                     width: 100,
+                                     height: 100)
+        userNameLabel.frame = CGRect(x: userImageView.righ + 10,
+                                     y: 10,
+                                     width: contentView.width - 20 - userImageView.width,
+                                     height: (contentView.height-20))
+       
+        
+    }
+    
+    public func configure(with model : Conversation){
+        
+        
+        self.userNameLabel.text = model.name
+        
+        let path = "images/\(model.otherUserEmail)_profile_picture.png"
+        
+        StorageManager.shared.downloadURL(for: path) { [weak self] result in
+            
+            switch result{
+            case .success(let url):
+                DispatchQueue.main.async {
+                    self?.userImageView.sd_setImage(with: url, completed: nil)
+                }
+                
+            case .failure(let error):
+                print("failed to get image url \(error)")
+            }
+        }
+        
+    }
+
+}
